02b0da2399279bc2db6812d5edc140f1c3311b5c
Mir Sole <zburn101@gmail.com>
2022-07-05 12:13:55 +1000

    fishe

---
+++ init.lua
@@ -0,0 +1,25 @@
+local nxml = dofile_once("mods/mould_n/lib/nxml.lua")
+
+function OnModInit()
+	print("Mod - OnModInit()")
+end
+
+function OnModPostInit()
+	print("Mod - OnModPostInit()")
+end
+
+function OnPlayerSpawned( player_entity )
+
+end
+
+function OnWorldPostUpdate()
+
+end
+
+function OnPlayerDied()
+	print("dead")
+end
+
+
+
+GamePrint("Fishing")
