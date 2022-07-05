72a5df444fc19625ddfdcd3b847df1e3f1ef2437
Mir Sole <zburn101@gmail.com>
2022-07-05 22:19:54 +1000

    push moment

--- files/fish.lua
+++ files/fish.lua
@@ -1,4 +1,7 @@
 dofile_once("data/scripts/lib/utilities.lua")
+dofile("mods/fishing_emporium/lib/stringstore.lua")
+dofile("mods/fishing_emporium/lib/noitavariablestore.lua")
+dofile("mods/fishing_emporium/files/fish_list.lua")
 
 function material_area_checker_success( pos_x, pos_y )
     local hook = GetUpdatedEntityID()
@@ -6,16 +9,15 @@ function material_area_checker_success( pos_x, pos_y )
         
     local wait_time = 0
 
+    local hooked_fish = {}
+
     local vel_comp = EntityGetFirstComponent(hook, "VelocityComponent")
     ComponentSetValue2(vel_comp, "mVelocity", 0, -50)
 
-    GamePrint("fishe")
-
-    --[[if (EntityGetTags(hook) == "") then
-
-    end]]--
+    --GamePrint("fishe")
 
     if (EntityHasTag(hook, "hook_t1")) then
+        hooked_fish = get_fish(1)
         
     end
 
@@ -28,9 +30,32 @@ function material_area_checker_success( pos_x, pos_y )
     end
     end
 
+    --wait
+
+
+    GamePrint(hooked_fish)
+    GamePrint(hooked_fish["id"])
+
+    EntityKill(hook)
+
     fish()
 end
 
 function fish()
-    GamePrint("function fish moment")
+
+end
+
+function get_fish(fish_tier)
+    GamePrint("get_fish")
+    shuffled = {}
+    for i, v in ipairs(fish_list) do
+    local pos = math.random(1, #shuffled+1)
+    table.insert(shuffled, pos, v)
+    end
+    for i, v in ipairs(shuffled) do 
+        local x = v["tier"] == fish_tier and i or 0;
+        if (x ~= 0) then return x
+    end
+    GamePrint("got_fish")
+end
 end
--- files/fish_list.lua
+++ files/fish_list.lua
@@ -5,10 +5,11 @@ fish_list =
         name = "Test Fish",
         desc = "This is a test fish.",
         sprite = "data/ui_gfx/gun_actions/freezing_gaze.png",
-        tier = "1",
+        tier = 1,
         biomes = {
             any,
         },
-        gold_awarded = "20"
+        gold_awarded = 20,
+        rare = false,
     },
 }
