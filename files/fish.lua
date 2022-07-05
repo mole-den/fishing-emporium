5e7fcae236c853121daf95647a7d5d60b3985c23
Oliver Fox-Smith <ofoxsmith@outlook.com>
2022-07-05 22:56:02 +1000

    e

--- files/fish.lua
+++ files/fish.lua
@@ -1,7 +1,7 @@
 dofile_once("data/scripts/lib/utilities.lua")
 dofile("mods/fishing_emporium/lib/stringstore.lua")
 dofile("mods/fishing_emporium/lib/noitavariablestore.lua")
-dofile("mods/fishing_emporium/files/fish_list.lua")
+dofile("mods/fishing_emporium/filesfish_list.lua")
 
 function material_area_checker_success( pos_x, pos_y )
     local hook = GetUpdatedEntityID()
@@ -46,16 +46,16 @@ function fish()
 end
 
 function get_fish(fish_tier)
-    GamePrint("get_fish")
-    shuffled = {}
+    local shuffled = {}
     for i, v in ipairs(fish_list) do
     local pos = math.random(1, #shuffled+1)
     table.insert(shuffled, pos, v)
     end
     for i, v in ipairs(shuffled) do 
-        local x = v["tier"] == fish_tier and i or 0;
-        if (x ~= 0) then return x
+        local x;
+        if v["tier"] == fish_tier then x = v else x = 0 end
+        if (x ~= 0) then return x 
     end
-    GamePrint("got_fish")
 end
 end
+
